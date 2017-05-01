c62a2ec2e723ba1dca71bf494a315a975ce03b3f
wlkemper1 <wlkemper@gmail.com>
2017-04-30 22:27:10 -0500

    photogram final HW Upload

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,31 +1,55 @@
 class PhotosController < ApplicationController
 
-  def show
+  def new_form
+    render("photos/new_form.html.erb")
+  end
 
+  def create_row
+    # render("photos/create_row.html.erb")
+    p = Photo.new
+    p.source = params["the_image_url"]
+    p.caption = params["the_caption"]
+    p.save
+    redirect_to("/photos")
+  end
+
+  def index
+    @all_photos = Photo.all.order({:created_at => :desc })
+    render("photos/index.html.erb")
+  end
+
+  def show
     # The params has looks like this: {"the_id_number"=>}
 
     id_number = params["the_id_number"]
 
-    p = Photo.find(id_number)
-
-    @the_caption = p.caption
-
-    @the_image_url = p.source
-
-    @created_at = p.created_at
-
-    render("show.html.erb")
+    @photo = Photo.find(id_number)
+    render("photos/show.html.erb")
   end
 
 
-  def index
-    @all_photos = Photo.all
-    render("photos/index.html.erb")
+  def edit_form
+    @photo = Photo.find(params[:the_id])
+    render ("photos/edit_form.html.erb")
   end
 
+  def update_row
+    p = Photo.find(params[:the_id])
 
+    p.source = params[:the_source]
+    p.caption = params[:the_caption]
+    p.save
 
+    @the_id = p.id
 
+    redirect_to("/photos/#{@the_id}")
+  end
 
+  def destroy_row
+    p = Photo.find(params[:the_id])
+    p.destroy
+    @remaining_count = Photo.count
+    redirect_to("/photos")
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
 
 
+  # CREATE
+  get("/photos/new", { :controller => "photos", :action => "new_form" })
+  get("photos/create_photo", { :controller => "photos", :action => "create_row" })
 
-  get("/photos", {:controller => "photos", :action => "index"})
-  get("/photos/:the_id_number", {:controller => "photos", :action => "show"})
+  # READ
+  get("/photos", { :controller => "photos", :action => "index" })
+  get("/", { :controller => "photos", :action => "index" })
+  get("/photos/:the_id_number", { :controller => "photos", :action => "show" })
+
+  # UPDATE
+  get("/photos/:the_id/edit", { :controller => "photos", :action => "edit_form" })
+  get("/update_photo/:the_id", { :controller => "photos", :action => "update_row" })
+
+  # DELETE
+  get("delete_photo/:the_id", { :controller => "photos", :action => "destroy_row" })
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
